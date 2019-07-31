31399923446bd69a043909cd8c25c368951401db
Monica Figuera <monicafigueray@gmail.com>
2019-07-31 14:40:37 +0200

    Data files merged (source-based), ML method column added

---
+++ data/ML method based interactions predictions/Filtered data/LapRLS/DDI/addColumns.py
@@ -0,0 +1,67 @@
+import os
+import glob
+import pandas as pd
+
+def read_csv(filename):
+    df = pd.read_csv(filename)
+    return df
+ 
+def return_method_column(df, method_name):
+    ML_method = []
+    total_rows = len(df['Drug1'])
+    for i in range(total_rows):
+        ML_method.append(method_name)
+    return ML_method
+
+def return_source_column(df, source_name):
+    source = []
+    total_rows = len(df['Drug1'])
+    for i in range(total_rows):
+        source.append(source_name)
+    return source
+
+def saveFile(data, file_name):
+
+    path = "./added_source_method_columns/"
+    #os.chdir(path)
+
+    try:
+        os.mkdir(path)
+    except OSError:
+        print ("Creation of the directory %s failed" % path)
+    else:
+        print ("Successfully created the directory %s " % path)
+
+
+    file_name = path + file_name
+    data.to_csv(file_name, encoding='utf-8', index=False)
+
+def main():
+    method_name = "LapRLS"
+
+    data = []
+
+    data.append({ "CRD": "CRD-filtered.csv" })
+    data.append({ "DrugBank": "DrugBank-filtered.csv" })
+    data.append({ "NCRD": "NCRD-filtered.csv" })
+    data.append({ "UMLS": "UMLS-filtered.csv" })
+    data.append({ "WP4": "WP4-filtered.csv" })
+    
+    for elem in data:
+        for key,value in elem.items():
+            source_name = key
+            file_name = value
+
+            df = pd.read_csv(file_name) 
+
+            source_column = return_method_column(df, source_name)
+            df['Source'] = source_column
+
+            ML_method_column = return_method_column(df, method_name)
+            df['Method'] = ML_method_column
+
+            saveFile(df, file_name)
+
+
+if __name__== "__main__":
+    main()
